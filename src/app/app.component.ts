540e348b0effc2a41aa7f645ab7b57be2eb9f567
MichaelS96 <m.schwittay96@web.de>
2025-03-20 12:23:33 +0100

    editcard component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { JoinComponent } from "./join/join.component";
+import { EditcardComponent } from "./editcard/editcard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, JoinComponent],
+  imports: [CommonModule, JoinComponent, EditcardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/editcard/editcard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editcard',
+  standalone: true,
+  imports: [],
+  templateUrl: './editcard.component.html',
+  styleUrl: './editcard.component.scss'
+})
+export class EditcardComponent {
+
+}
