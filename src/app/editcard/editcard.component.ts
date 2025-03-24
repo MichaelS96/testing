91bfa8b5081ec9337fd3519ff8a704322844c53d
MichaelS96 <m.schwittay96@web.de>
2025-03-24 09:39:53 +0100

    editcard dropdown added

--- src/app/editcard/editcard.component.ts
+++ src/app/editcard/editcard.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-editcard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './editcard.component.html',
   styleUrl: './editcard.component.scss'
 })
 export class EditcardComponent {
+  dropdownVisible = false;
 
+  toggleDropdown() {
+    this.dropdownVisible = !this.dropdownVisible;
+  }
 }
